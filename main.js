51a4c361189a66040b74c05c5e19c3695f59df7e
Ana Gabriela <anagabrieladev@gmail.com>
2021-10-15 23:54:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function gerarTabuada(){
+    let num = document.getElementById("txtNumber");
+    let tabuada = document.getElementById("selTabuada");
+
+    if (num.value.length == 0){
+        window.alert("Por favor, Digite um numero :)")
+    } else {
+        let n = Number(num.value);
+        let c = 1;
+        tabuada.innerHTML = ''
+        while (c <= 10){
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n * c}`;
+            tabuada.appendChild(item);
+            c++;
+        }
+    } 
+}
